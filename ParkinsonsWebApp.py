e248321d44e09708e951d7b24c43a9759294bfaa
trazhboi <k.kashyap.x@gmail.com>
2026-03-22 17:15:56 +0530

    Update ParkinsonsWebApp.py

--- ParkinsonsWebApp.py
+++ ParkinsonsWebApp.py
@@ -6,86 +6,82 @@ import streamlit as st
 # loading the saved model
 loaded_model = pickle.load(open('D:/cse/parkinsons/trained_model.sav', 'rb'))
 
-
-# Parkinson's Prediction Page
-if selected == "Parkinsons Prediction":
-
     # page title
-    st.title("Parkinson's Disease Prediction using ML")
+st.title("Parkinson's Disease Prediction using ML")
 
     col1, col2, col3, col4, col5 = st.columns(5)
 
-    with col1:
+with col1:
         fo = st.text_input('MDVP:Fo(Hz)')
 
-    with col2:
+with col2:
         fhi = st.text_input('MDVP:Fhi(Hz)')
 
-    with col3:
+with col3:
         flo = st.text_input('MDVP:Flo(Hz)')
 
-    with col4:
+with col4:
         Jitter_percent = st.text_input('MDVP:Jitter(%)')
 
-    with col5:
+with col5:
         Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
 
-    with col1:
+with col1:
         RAP = st.text_input('MDVP:RAP')
 
-    with col2:
+with col2:
         PPQ = st.text_input('MDVP:PPQ')
 
-    with col3:
+with col3:
         DDP = st.text_input('Jitter:DDP')
 
-    with col4:
+with col4:
         Shimmer = st.text_input('MDVP:Shimmer')
 
-    with col5:
+with col5:
         Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
 
-    with col1:
+with col1:
         APQ3 = st.text_input('Shimmer:APQ3')
 
-    with col2:
+with col2:
         APQ5 = st.text_input('Shimmer:APQ5')
 
-    with col3:
+with col3:
         APQ = st.text_input('MDVP:APQ')
 
-    with col4:
+with col4:
         DDA = st.text_input('Shimmer:DDA')
 
-    with col5:
+with col5:
         NHR = st.text_input('NHR')
 
-    with col1:
+with col1:
         HNR = st.text_input('HNR')
 
-    with col2:
+with col2:
         RPDE = st.text_input('RPDE')
 
-    with col3:
+with col3:
         DFA = st.text_input('DFA')
 
-    with col4:
+with col4:
         spread1 = st.text_input('spread1')
 
-    with col5:
+with col5:
         spread2 = st.text_input('spread2')
 
-    with col1:
+with col1:
         D2 = st.text_input('D2')
 
-    with col2:
+with col2:
         PPE = st.text_input('PPE')
 
-    # code for Prediction
+# code for Prediction
     parkinsons_diagnosis = ''
 
     # creating a button for Prediction    
-    if st.button("Parkinson's Test Result"):
+if st.button("Parkinson's Test Result"):
 
         user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
                       RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
@@ -93,11 +89,11 @@ if selected == "Parkinsons Prediction":
 
         user_input = [float(x) for x in user_input]
 
-        parkinsons_prediction = parkinsons_model.predict([user_input])
+        parkinsons_prediction = loaded_model.predict([user_input])
 
-        if parkinsons_prediction[0] == 1:
+if parkinsons_prediction[0] == 1:
             parkinsons_diagnosis = "The person has Parkinson's disease"
-        else:
+else:
             parkinsons_diagnosis = "The person does not have Parkinson's disease"
 
     st.success(parkinsons_diagnosis)
