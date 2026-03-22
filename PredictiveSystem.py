a8e7e2e0a8d25473b02fcc1024c431fde849e8b5
trazhboi <k.kashyap.x@gmail.com>
2026-03-22 16:35:39 +0530

    Add files via upload

---
+++ PredictiveSystem.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pickle
+
+# loading the saved model
+loaded_model = pickle.load(open('D:/cse/parkinsons/trained_model.sav', 'rb'))
+
+
+input_data = (197.07600,206.89600,192.05500,0.00289,0.00001,0.00166,0.00168,0.00498,0.01098,0.09700,0.00563,0.00680,0.00802,0.01689,0.00339,26.77500,0.422229,0.741367,-7.348300,0.177551,1.743867,0.085569)
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the numpy array
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+# standardize the data
+std_data = scalar.transform(input_data_reshaped)
+
+prediction = loaded_model.predict(std_data)
+print(prediction)
+
+
+if (prediction[0] == 0):
+  print("The Person does not have Parkinsons Disease")
+
+else:
+  print("The Person has Parkinsons Disease")
